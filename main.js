526816f5af398d07837a037396d446eb17b74494
DavidDevGt <josuedavidvl18@gmail.com>
2023-10-30 00:25:23 -0600

    frontend v0.1 terminado

---
+++ main.js
@@ -0,0 +1,8 @@
+// El navigo
+const router = new Navigo('/');
+
+// Refs de las páginas
+const loginPage = document.getElementById('login');
+const registerPage = document.getElementById('register');
+const welcomePage = document.getElementById('welcome');
+
